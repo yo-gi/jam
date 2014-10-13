580081e3366161d0d68c4200b77caeb1e6ed3ef0
jyh947 <jyh947@gmail.com>
2014-10-13 17:14:49 -0400

    Enemy #2 moves in a square

--- src/Enemy_02.js
+++ src/Enemy_02.js
@@ -5,8 +5,8 @@ var Enemy_02 = cc.Sprite.extend({
         this._super();
         this.init();
         this.action = 0;
-        this.x = 0;
-        this.y = 0;
+        this.x = 200;
+        this.y = 200;
         this.x_prev = 0;
         this.y_prev = 0;
         this.gameTicks = 0;
@@ -18,17 +18,32 @@ var Enemy_02 = cc.Sprite.extend({
     update:function(dt){
         this.x_prev = this.x;
         this.y_prev = this.y;
-        this.x = this.x + 5;
-        this.y = (250 * Math.sin((this.gameTicks * 0.5 * Math.PI)/80)) + 280;
+        if((this.gameTicks % 600) < 150)
+        {
+            this.y = (this.gameTicks % 150) + 200;
+        }
+        else if((this.gameTicks % 600) < 300)
+        {
+            this.x = (this.gameTicks % 150) + 200;
+        }
+        else if((this.gameTicks % 600) < 450)
+        {
+            this.y = 350 - (this.gameTicks % 150);
+        }
+        else
+        {
+            this.x = 350 - (this.gameTicks % 150);
+        }
+        //cc.log(this.y + " " + this.x);
         this.gameTicks++;
         if(this.x >= 1250)
         {
-            this.x = 0;
+            this.x = 1250;
         }
- /*       else if(this.x <= 30)
+        else if(this.x <= 30)
         {
             this.x = 30;
-        }*/
+        }
 
         if(this.y >= 710)
         {
