fde514d42dff7a31c955b6950d41e50833fce2a1
jyh947 <jyh947@gmail.com>
2014-10-12 22:38:39 -0400

    Player Movement

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -2,6 +2,7 @@ var PlayScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
         //add three layer in the right order
+        this.addChild(new GameLayer());
         this.addChild(new BackgroundLayer());
         this.addChild(new Player());
         this.addChild(new StatusLayer());
--- src/Player.js
+++ src/Player.js
@@ -1,13 +1,39 @@
-var Player = cc.Layer.extend({
+var Player = cc.Sprite.extend({
+    currentRotation:0,
+    spriteRunner:null,
     ctor:function () {
+        var winsize = cc.director.getWinSize();
         this._super();
         this.init();
+        this.x = winsize.width / 2;
+        this.y = winsize.height / 2;
+        //W = 87
+        //A = 65
+        //S = 83
+        //D = 68
+    },
+    update:function(dt){
+        if ((MW.KEYS[cc.KEY.w] || MW.KEYS[cc.KEY.up]) && this.y <= 600) {
+            this.y += 10;
+        }
+        if ((MW.KEYS[cc.KEY.s] || MW.KEYS[cc.KEY.down]) && this.y >= 0) {
+            this.y -= 10;
+        }
+        if ((MW.KEYS[cc.KEY.a] || MW.KEYS[cc.KEY.left]) && this.x >= 0) {
+            this.x -= 10;
+        }
+        if ((MW.KEYS[cc.KEY.d] || MW.KEYS[cc.KEY.right]) && this.x <= 800) {
+            this.x += 10;
+        }
+
     },
     init:function () {
         this._super();
 
         //create the hero sprite
-        var spriteRunner = new cc.Sprite(res.Player_png);
-        spriteRunner.attr({x: 400, y: 300});
+        this.spriteRunner = new cc.Sprite(res.Player_png);
+        this.addChild(this.spriteRunner);
+        this.spriteRunner.setPosition(new cc.Point(this.x,this.y));
+        this.schedule(this.update);
     }
 });
